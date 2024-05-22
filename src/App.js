f6d9df94a728380b2ec9c2487b5c0bb75fdf2f77
Pranshu-15 <pranshu923@gmail.com>
2024-05-22 20:10:16 +0530

    initiated the project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Components/Common/Header';
+import MainComponent from './Components/LandingPage/MainComponent';
+
+const App = () => {
+  return (
+  <>
+  <Header/>
+  <MainComponent/>
+  </>
+  );
+}
+
+export default App;
---
+++ src/Components/Common/Header/drawer.js
@@ -0,0 +1,37 @@
+import Drawer from "@mui/material/Drawer";
+import { useState } from "react";
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import { IconButton } from "@mui/material";
+import './style.css'
+import Button from "../Buttons";
+const  MobileNav = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div>
+      <IconButton onClick={() => setOpen(true)}>
+        <MenuRoundedIcon className="link"/>
+      </IconButton>
+      <Drawer 
+      anchor={"right"} 
+       open={open} 
+       onClose={() => setOpen(false)}>
+        <div className="drawer-div">
+        <a href="/">
+          <p className="link">Home</p>
+        </a>
+        <a href="/compare">
+          <p className="link">Compare</p>
+        </a>
+        <a href="/watchlist">
+          <p className="link">Watchlist</p>
+        </a>
+        <a href="/dashboard">
+          <Button text={"dashboard"} />
+        </a>
+        </div>
+      </Drawer>
+    </div>
+  );
+}
+export default MobileNav;
